a83fe2a91ea86ac4d7e7b90f827e0d3dbf69fdf5
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-05-08 14:13:59 +0100

    Defining the variables

---
+++ app.js
@@ -0,0 +1,6 @@
+// Defining the variables
+const form = document.querySelector('#task-form')
+const tasklist = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
